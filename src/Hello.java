aea9c6564b3ac8196ff67293f25554559ecba913
Alejandro Reyes <jandrolo94@gmail.com>
2016-08-19 03:03:02 +0200

    commit2

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		// cambio 2
+		// cambio3
 		// Prints "Hello, World" to the terminal window.
 		System.out.println("Hello, World");
 	}
